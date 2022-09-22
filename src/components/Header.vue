e6e4d6518e59c6cd113dab3c1d2d4e1813aed829
Dean Huang <hcd@mojopot.com>
2022-09-22 14:55:39 +0800

    remove lint & build header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 <header class="pl-5 flex items-center">
-  <img src='https://1000logos.net/wp-content/uploads/2017/12/Bing-logo.png' alt="falcon" class="w-32"/>
-  <input type="search" class="pl-2" />
+  <img src='https://1000logos.net/wp-content/uploads/2017/12/Bing-logo.png' alt="falcon" class="w-44"/>
+  <input type="search" class="ml-3 pl-2 py-1 rounded-[100px] w-96 drop border border-gray-400" placeholder="熱門報導" />
 </header>
 </template>
 
